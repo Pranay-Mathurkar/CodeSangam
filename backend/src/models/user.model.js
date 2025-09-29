f214ff59a51e56d6cec5f485e7f1c8f057b9a432
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-09-29 18:40:45 +0530

    changes in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,14 +1,11 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema(
-    {
-        name: { type: String, required: true },
-       email: { type: String, unique: true },
-        password: { type: String, required: true },
-        token: { type: String }
-    }
-)
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String },    
+  googleId: { type: String }     
+});
 
 const User = mongoose.model("User", userSchema);
-
 export { User };
