6d59f8d06c9770623d97d3f8b4bd96be9760e182
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-09-29 15:55:44 +0530

    connected to database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,39 @@
 import express from 'express';
 import cors from 'cors';
-//import {CreateServer} from "node:http";
+
 import mongoose from "mongoose";
 
 
 
 const app = express();
 
+
+app.set("port", (process.env.PORT || 3000))
 app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
 
 
 app.get('/home', (req, res) => {
     res.send('Hello World!');
 });
 
-const start =async () => {
-    app.listen(3000, () => {
+
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://pranaymnnit123_db_user:wm0O5BYQUaLNE73u@cluster0.morcz9e.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+     app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
+
+
+
 }
+
+
+
 start();
