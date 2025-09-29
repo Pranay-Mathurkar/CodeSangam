5c6d8e0048c1d749c43ddb5155904b06648fc8b0
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-09-29 16:33:23 +0530

    make routes

---
+++ backend/src/routes/user.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { addToHistory,login, register } from "../controllers/user.controller.js";
+
+
+
+const router = Router();
+
+router.route("/login").post(login)
+router.route("/register").post(register)
+router.route("/add_to_activity").post(addToHistory)
+
+export default router;
