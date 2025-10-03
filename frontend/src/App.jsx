a7031bbf9e940b7ca16a3e05fa8c31020a47baae
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-10-03 17:31:27 +0530

    extra pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,44 @@
 
-
-
-
-
-
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 
-// Page Imports
+
 import Register from "./pages/register";
 import Login from "./pages/login";
 import Home from "./pages/home";
+import Support from "./pages/support";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import Documentation from "./pages/documentation";
+import User from "./pages/user";
+
+
 
 export default function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          {/* Default route redirect */}
+        
+
           <Route path="/" element={<Navigate to="/login" replace />} />
 
-          {/* Auth routes */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
 
-          {/* Protected / main route */}
+         
+
+
           <Route path="/home" element={<Home />} />
+            <Route path="/support" element={<Support />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/documentation" element={<Documentation />} />
+          <Route path="/user" element={<User />} />
+
+
+
+         
         </Routes>
       </AuthProvider>
     </Router>
